ca82d10e436580ac37c9adce7625e88202154f51
maxels95 <mikael.axelsson@protonmail.com>
2024-05-09 20:44:50 +0200

    Changed to using React instead

---
+++ AgriWeatherTracker/Frontend/agri_web/src/LineGraph.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LineGraph() {
+  return (
+    <div>
+      <h2>Line Graph Placeholder</h2>
+      {/* This will later be replaced with actual graph rendering logic */}
+    </div>
+  );
+}
+
+export default LineGraph;
