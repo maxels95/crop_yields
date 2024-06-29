2d7f10beb3213ee2d1c700fcea0e33526bd065cd
maxels95 <mikael.axelsson@protonmail.com>
2024-06-29 21:20:15 +0200

    Preparations for Azure deployment.

--- AgriWeatherTracker/API/Program.cs
+++ AgriWeatherTracker/API/Program.cs
@@ -47,8 +47,14 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
 var app = builder.Build();
+
+// Apply migrations at startup
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
