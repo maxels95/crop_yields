016d5128c9140891c75cc684fdc3fd2884834d2e
maxels95 <mikael.axelsson@protonmail.com>
2024-12-27 20:52:41 +0100

    Added py script to auto fetch and pull changes on the PI

---
+++ AgriWeatherTracker/AutoRun/fetchPull.py
@@ -0,0 +1,19 @@
+import os
+from git import Repo
+
+# Path to the repository
+repo_path = repo_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+# Open the repository
+repo = Repo(repo_path)
+
+# Fetch the latest changes
+upstream = repo.remotes.upstream
+upstream.fetch()
+
+# Check if there are new commits to pull
+if repo.head.commit != repo.refs['upstream/main'].commit:
+    print("New changes found. Pulling updates...")
+    upstream.pull()
+else:
+    print("No changes detected.")
